de5cf28c1d865721d2e19bb40b799c6b4f15aca2
tjehddnr <tjehddnr90@gmail.com>
2022-08-28 13:31:27 +0900

    테스트 코드 작성

---
+++ src/main/java/com/seo/demo/constant/ItemSellStatus.java
@@ -0,0 +1,5 @@
+package com.seo.demo.constant;
+
+public enum ItemSellStatus {
+    SELL, SOLD_OUT
+}
---
+++ src/main/java/com/seo/demo/repository/ItemRepository.java
@@ -0,0 +1,11 @@
+package com.seo.demo.repository;
+
+import com.seo.demo.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);
+}
